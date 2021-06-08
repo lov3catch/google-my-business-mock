b8b9327f70e9e0cdd53d8b9f7a1c66a7615de56a
Ihor K <lov3catch@gmail.com>
2021-06-08 11:34:26 +0300

    - move files to src/

---
+++ src/GoogleMyBusinessFakeHttpClient.php
@@ -0,0 +1,52 @@
+<?php
+
+declare(strict_types=1);
+
+namespace GoogleMyBusinessMock;
+
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+use Symfony\Contracts\HttpClient\ResponseInterface;
+use Symfony\Contracts\HttpClient\ResponseStreamInterface;
+
+
+/**
+ * @method withOptions(array $options)
+ */
+class GoogleMyBusinessFakeHttpClient implements HttpClientInterface
+{
+    public function request(
+        string $method,
+        string $url,
+        array $options = []
+    ): ResponseInterface {
+        if ('https://mybusiness.googleapis.com/v4/accounts/108494581129006735086/locations' === $url && 'GET' === $method) {
+            return ResponseFactory::makeLocationListResponse($method, $url, $options);
+        }
+
+        if ('https://mybusiness.googleapis.com/v4/accounts/108494581129006735086/locations/6394464039148850881/reviews' === $url && 'GET' === $method) {
+            return ResponseFactory::makeReviewListResponse($method, $url, $options);
+        }
+
+        if ('https://mybusiness.googleapis.com/v4/accounts/108494581129006735086/locations/13195921940790549583/reviews/AbFvOqmDfUP0EhJuYm-w3KXy3gSxYn-NS9CiDOo4tOjO4PRuq4PWUWbGkh6K-fePdKQaXi0cycFGGg' === $url && 'GET' === $method) {
+            return ResponseFactory::makeReviewDetailResponse($method, $url, $options);
+        }
+
+        if ('https://mybusiness.googleapis.com/v4/accounts/108494581129006735086/locations/6394464039148850881/questions' === $url && 'GET' === $method) {
+            return ResponseFactory::makeQuestionListResponse($method, $url, $options);
+        }
+
+        if ('https://mybusiness.googleapis.com/v4/accounts/108494581129006735086/locations/13195921940790549583/questions/AIe9_BF8X4EpR_fF6mwcYdso9awgNBXZYS-R4EHEW06LmxMr91KSWl_9vqMChjmP_lpnINp1kxXgkp5abmOdyb03He-ESr6SJkSiDuR9p25gq0NZ3dwvC7bgj0LUTqKnjuhhBXVth9ys/answers' === $url && 'GET' === $method) {
+            return ResponseFactory::makeQuestionListResponse($method, $url, $options);
+        }
+    }
+
+    public function stream($responses, float $timeout = null): ResponseStreamInterface
+    {
+        // TODO: Implement stream() method.
+    }
+
+    public function __call(string $name, array $arguments)
+    {
+        // TODO: Implement @method  withOptions(array $options)
+    }
+}
---
+++ src/ResponseFactory.php
@@ -0,0 +1,94 @@
+<?php
+
+declare(strict_types=1);
+
+
+namespace GoogleMyBusinessMock;
+
+
+use Symfony\Component\HttpClient\Response\MockResponse;
+use Symfony\Contracts\HttpClient\ResponseInterface;
+
+class ResponseFactory
+{
+    public static function makeAccountListResponse(
+        string $method,
+        string $url,
+        array $options
+    ): ResponseInterface {
+    }
+
+    public static function makeLocationListResponse(
+        string $method,
+        string $url,
+        array $options
+    ): ResponseInterface {
+        return MockResponse::fromRequest(
+            $method,
+            $url,
+            $options,
+            new MockResponse(
+                file_get_contents(__DIR__ . '/mock/location_list_response.json')
+            )
+        );
+    }
+
+    public static function makeReviewListResponse(
+        string $method,
+        string $url,
+        array $options
+    ): ResponseInterface {
+        return MockResponse::fromRequest(
+            $method,
+            $url,
+            $options,
+            new MockResponse(
+                file_get_contents(__DIR__ . '/mock/review_list_response.json')
+            )
+        );
+    }
+
+    public static function makeReviewDetailResponse(
+        string $method,
+        string $url,
+        array $options
+    ): ResponseInterface {
+        return MockResponse::fromRequest(
+            $method,
+            $url,
+            $options,
+            new MockResponse(
+                file_get_contents(__DIR__ . '/mock/review_detail_response.json')
+            )
+        );
+    }
+
+    public static function makePostListResponse(
+        string $method,
+        string $url,
+        array $options
+    ): ResponseInterface {
+    }
+
+    public static function makeQuestionListResponse(
+        string $method,
+        string $url,
+        array $options
+    ): ResponseInterface {
+        return MockResponse::fromRequest(
+            $method,
+            $url,
+            $options,
+            new MockResponse(
+                file_get_contents(__DIR__ . '/mock/question_list_response.json')
+            )
+        );
+    }
+
+    public static function makeAnswerListResponse(
+        string $method,
+        string $url,
+        array $options
+    ): ResponseInterface {
+    }
+}
